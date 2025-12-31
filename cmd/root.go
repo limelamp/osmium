d50a47a3a604e6bdcfa0861380060306b720f4be
turalto <gulumovt143@gmail.com>
2026-01-01 03:55:05 +0400

    Basic downloading functionality skeleton added

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -34,14 +36,64 @@ func initializedDashboardModel() dashboardModel {
 	return dashboardModel{cursor: 0}
 }
 
-// Setup data ----------------------------------------------------------------
+// Setup data -------------------------------------------------------------------------S
 type setupModel struct {
-	cursor int
+	cursor     int
+	step       int
+	jarType    string
+	jarVersion string
+	options    []string
 }
 
 // initialized dashboard model
 func initializedSetupModel() setupModel {
-	return setupModel{cursor: 0}
+	return setupModel{
+		cursor:  0,
+		options: []string{"Vanilla", "Bukkit", "Spigot", "Paper", "Purpur"},
+	}
+}
+
+func downloadJar(jarType string, jarVersion string) {
+	// Deciding which url
+	url := ""
+	switch jarType {
+	case "Vanilla":
+		url = "https://piston-data.mojang.com/v1/objects/64bb6d763bed0a9f1d632ec347938594144943ed/server.jar"
+	case "Bukkit":
+		url = ""
+	case "Spigot":
+		url = ""
+	case "Paper":
+		url = "https://api.papermc.io/v2/projects/paper/versions/" + jarVersion
+	case "Purpur":
+		url = "https://api.purpurmc.org/v2/purpur/" + jarVersion + "/latest/download"
+	}
+
+	output := "server.jar"
+
+	fmt.Println("Downloading the required files....")
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		panic(fmt.Sprintf("bad status: %s", resp.Status))
+	}
+
+	out, err := os.Create(output)
+	if err != nil {
+		panic(err)
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Download finished: ", output)
 }
 
 // States ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -60,10 +112,10 @@ func (m rootModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.setup = newSetup
 		cmd = newCmd
 
-		// Check for the `server.jar` file's existance
-		// if _, err := os.Stat("server.jar"); err == nil {
-		// 	m.state = stateDashboard
-		// }
+		//Check for the `server.jar` file's existance in the background
+		if _, err := os.Stat("server.jar"); err == nil {
+			m.state = stateDashboard
+		}
 
 	case stateDashboard:
 		newDash, newCmd := m.dashboard.Update(msg)
@@ -79,6 +131,83 @@ func (m rootModel) View() string {
 		return m.setup.View()
 	}
 	return m.dashboard.View()
+}
+
+// Setup State ----------------------------------------------------------------------------------
+// Handles the setup model's data and all actions
+func (m setupModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m setupModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down":
+			if m.cursor < len(m.options)-1 {
+				m.cursor++
+			} // assuming 4 options
+		case "enter":
+			switch m.step {
+			case 0:
+				m.jarType = m.options[m.cursor] // Save the type
+
+				// Now change the options to Versions
+				m.step = 1
+				m.cursor = 0
+				m.options = []string{"1.21.11", "1.21", "1.20", "1.19"}
+
+			case 1:
+				m.jarVersion = m.options[m.cursor] // Save the version
+
+				// Move to download state
+				m.step = 2
+				// return m, downloadServerJar(m.serverType, m.version)
+			case 2:
+				downloadJar(m.jarType, m.jarVersion)
+			}
+		}
+
+	}
+	return m, nil
+}
+
+// Basically a big print function huh
+func (m setupModel) View() string {
+	// Style your header with Lip Gloss
+	headerStyle := lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color("#FAFAFA")).
+		Background(lipgloss.Color("#63f456ff")).
+		Padding(0, 1)
+
+	s := headerStyle.Render(" OSMIUM SERVER INITIALIZATION ") + "\n\n"
+	s += "There appears to be no server initialized in the current folder!" + "\n"
+	s += "This setup wizard will be guiding you through the creation of the server." + "\n\n"
+
+	switch m.step {
+	case 0:
+
+	}
+	// Create a simple list
+	// serverTypes := [length]string{"Vanilla", "Bukkit", "Spigot", "Paper", "Purpur"}
+	for i := 0; i < len(m.options); i++ {
+		cursor := "  "
+		if m.cursor == i {
+			cursor = "> "
+		}
+		s += fmt.Sprintf("%s %s\n", cursor, m.options[i])
+	}
+
+	s += "\n\n" + "Navigate using arrow keys. Press 'q' to exit.\n\n"
+	return s
 }
 
 // Dashboard State --------------------------------------------------------------------------------------------------------
@@ -127,59 +256,6 @@ func (m dashboardModel) View() string {
 		s += fmt.Sprintf("%s Option %d\n", cursor, i)
 	}
 
-	return s
-}
-
-// Setup State ----------------------------------------------------------------------------------
-// Handles the setup model's data and all actions
-func (m setupModel) Init() tea.Cmd {
-	return nil
-}
-
-func (m setupModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		case "up":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-		case "down":
-			if m.cursor < 4 {
-				m.cursor++
-			} // assuming 4 options
-		}
-	}
-	return m, nil
-}
-
-// Basically a big print function huh
-func (m setupModel) View() string {
-	// Style your header with Lip Gloss
-	headerStyle := lipgloss.NewStyle().
-		Bold(true).
-		Foreground(lipgloss.Color("#FAFAFA")).
-		Background(lipgloss.Color("#63f456ff")).
-		Padding(0, 1)
-
-	s := headerStyle.Render(" OSMIUM SERVER INITIALIZATION ") + "\n\n"
-	s += "There appears to be no server initialized in the current folder!" + "\n"
-	s += "This setup wizard will be guiding you through the creation of the server." + "\n\n"
-
-	// Create a simple list
-	const length int = 5
-	serverTypes := [length]string{"Vanilla", "Bukkit", "Spigot", "Paper", "Purpur"}
-	for i := 0; i < length; i++ {
-		cursor := "  "
-		if m.cursor == i {
-			cursor = "> "
-		}
-		s += fmt.Sprintf("%s %s\n", cursor, serverTypes[i])
-	}
-
-	s += "\n\n" + "Navigate using arrow keys. Press 'q' to exit.\n\n"
 	return s
 }
 
